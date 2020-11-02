c202014c9bce7d23730b3360ff3e31356af25edc
vuquockhanh124 <54862143+vuquockhanh124@users.noreply.github.com>
2020-11-02 01:56:27 -0600

    adding more script for mob animation

---
+++ Assets/Scripts/Khanh/Player.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class Player : MonoBehaviour
+{
+    public Rigidbody2D r2;
+    public Animator anim;
+
+    public int ourHealth;
+    public int maxhealth = 5;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        r2 = gameObject.GetComponent<Rigidbody2D>();
+        anim = gameObject.GetComponent<Animator>();
+        ourHealth = maxhealth;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void FixedUpdate()
+    {
+        if (ourHealth <= 0)
+        {
+            Death();
+        }
+    }
+
+    public void Death()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);  // put player back to first spawn location
+    }
+
+    public void Damage(int damage)
+    {
+        ourHealth -= damage;
+      //  gameObject.GetComponent<Animation>().Play("redflash");
+      // need a sprite for display player get damaged
+    }
+
+}
---
+++ Assets/Scripts/Khanh/bullet.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bullet : MonoBehaviour
+{
+    public float lifetime = 1;  // time visible of bullet
+
+
+    void Start()
+    {
+
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.isTrigger == false)
+        {
+            if (col.CompareTag("Player"))  // check is it player object
+            {
+                col.SendMessageUpwards("Damage", 1);
+            }
+            Destroy(gameObject);
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        lifetime -= Time.deltaTime;
+        if (lifetime <= 0)
+            Destroy(gameObject);
+    }
+}
