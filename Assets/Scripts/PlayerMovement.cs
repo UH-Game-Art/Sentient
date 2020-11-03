532a5a0a73e62e90993067194a1ac9d1361cd8c4
isaac <idreyes@uh.edu>
2020-11-02 19:28:55 -0600

    animations are working now

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -15,6 +15,7 @@ public class PlayerMovement : MonoBehaviour
     bool jump = false;
     bool crouch = false;
     // Update is called once per frame
+
     void Update()
     {
       horizontalMove = Input.GetAxisRaw("Horizontal")* runSpeed;
