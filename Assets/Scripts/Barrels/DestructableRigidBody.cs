d45b4551aba95bc03bd41afa10a0759a23bdcc00
Hin Vuong <54330598+HinV@users.noreply.github.com>
2020-11-07 14:48:11 -0600

    Barrels

---
+++ Assets/Scripts/Barrels/DestructableRigidBody.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestructableRigidBody : MonoBehaviour
+{
+    [SerializeField]
+    Vector2 forceDirection;
+
+    [SerializeField]
+    float torque;
+
+    Rigidbody2D rb2d;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+        rb2d.AddForce(forceDirection);
+        rb2d.AddTorque(torque);
+      
+    }
+
+   
+}
