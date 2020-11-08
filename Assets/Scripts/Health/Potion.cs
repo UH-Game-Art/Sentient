80a0a785d6721a38adc742ad22fec557937a1b1a
Hin Vuong <54330598+HinV@users.noreply.github.com>
2020-11-08 09:11:53 -0600

    potion

--- Assets/Scripts/Health/Potion.cs
+++ Assets/Scripts/Health/Potion.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class Potion : MonoBehaviour
 {
 
-    public int item_hp = 3;
+    public int item_hp = 2;
     private void OnTriggerEnter2D(Collider2D collision)
     {
 
