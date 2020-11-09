cd2382083209878c19c475923ad758a1daa67b9c
KiraKamii <70298396+KiraKamii@users.noreply.github.com>
2020-11-08 19:51:04 -0600

    finished Gamover and LifeUP
    added life system, checkpoint system, respawn system, and a temp gameover screen that will go back to the main menu after 5 seconds.

--- Assets/Scripts/Character/LifeSystem.cs
+++ Assets/Scripts/Character/LifeSystem.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class LifeSystem : MonoBehaviour
 {
@@ -10,18 +11,42 @@ public class LifeSystem : MonoBehaviour
     private int lifeCounter;
 
     private Text lifeText;
+
+    public GameObject GameOver;
+
+    public float WaitGameOver;
+    
+
     // Start is called before the first frame update
     void Start()
     {
         lifeText = GetComponent<Text>();
-
         lifeCounter = startingLives;
+
+        
     }
 
     // Update is called once per frame
     void Update()
     {
+        if(lifeCounter < 1)
+        {
+            GameOver.SetActive(true);
+            Time.timeScale = 0.1f;
+        }
+        
         lifeText.text = "x " + lifeCounter;
+
+        if(GameOver.activeSelf)
+        {
+            WaitGameOver -= Time.deltaTime;
+        }
+
+        if(WaitGameOver < 0)
+        {
+            Time.timeScale = 1f;
+            SceneManager.LoadScene("MainMenu");
+        }
     }
 
     public void LifeUP()
