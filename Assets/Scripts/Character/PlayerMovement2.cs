d670e0317e3f8e01dc2ddfd20e6496887288be6b
vuquockhanh124 <54862143+vuquockhanh124@users.noreply.github.com>
2020-11-10 14:11:45 -0600

    fixing from main

---
+++ Assets/Scripts/Character/PlayerMovement2.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement2 : MonoBehaviour
+{
+
+    public CharacterController2D controller;
+    public Animator animator;
+
+
+    float horizontalMove = 0f;
+    public float runSpeed = 40f;
+
+    bool jump = false;
+    bool crouch = false;
+    // Update is called once per frame
+
+    void Update()
+    {
+        AudioManager.instance.PlaySound(AudioManager.instance.theme2, 0.7f,true);
+        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+        animator.SetFloat("Speed", Mathf.Abs(horizontalMove));
+
+        if (Input.GetButtonDown("Jump"))
+        {
+
+            AudioManager.instance.PlaySound(AudioManager.instance.jump, 1);
+            jump = true;
+            animator.SetBool("IsJumping", true);
+        }
+
+        if (Input.GetButtonDown("Crouch"))
+        {
+            crouch = true;
+        }
+        else if (Input.GetButtonUp("Crouch"))
+        {
+            crouch = false;
+        }
+    }
+
+    public void OnLanding()
+    {
+        animator.SetBool("IsJumping", false);
+    }
+
+    void FixedUpdate()
+    {
+        controller.Move(horizontalMove * Time.fixedDeltaTime, crouch, jump);
+        jump = false;
+
+    }
+
+}
