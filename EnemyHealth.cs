c4c2618321e92b7e90b94aaf56410df80da90bd7
Daniel Aguila <47072469+Daniel-Aguila@users.noreply.github.com>
2020-11-11 06:17:39 -0600

    Add files via upload

---
+++ EnemyHealth.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHealth : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public int health = 10;
+    public EnemyBehavior enemyBeh;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (health <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+    public void damage(int n)
+    {
+        health -= n;
+    }
+}
