082881fc3636c1de5f703af10dc3822cef324987
yoretessma <yoretessma@gmail.com>
2020-11-14 08:02:21 -0600

    Boss_fight added
    boss_fight

---
+++ Assets/Assets/Scripts/Barrels/BarrelsScript.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BarrelsScript : MonoBehaviour
+{
+    public Bullet bullet_;
+    [SerializeField]
+    int health = 3;
+
+    [SerializeField]
+    UnityEngine.Object destructableRef;
+
+    //materials
+    private Material matwhite;
+    private Material matDefault;
+    SpriteRenderer sr;
+
+    void Start()
+    {
+        sr = GetComponent<SpriteRenderer>();
+        matwhite = Resources.Load("whiteflash", typeof(Material)) as Material;
+        matDefault = sr.material;
+    }
+
+    
+    
+        private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag.Equals("Bullet"))
+        {
+            Destroy(collision.gameObject);
+            health--;
+            sr.material = matwhite;
+
+            if (health <= 0)
+            {
+                ExplodeThisGameObject();
+            }
+            else
+            {
+                Invoke("ResetMaterial", .05f);
+            }
+
+        }
+
+    }
+
+    void ResetMaterial()
+    {
+        sr.material = matDefault;
+    }
+
+    private void ExplodeThisGameObject()
+    {
+        
+        GameObject destructable = (GameObject)Instantiate(destructableRef);
+
+
+        //map the newly loaded destructable object to the x and y position of the previously destroyed barrel
+
+        destructable.transform.position = transform.position;
+
+        Destroy(gameObject);
+       
+    }
+
+
+}
---
+++ Assets/Assets/Scripts/Character/LifeSystem.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class LifeSystem : MonoBehaviour
+{
+    
+    public int startingLives;
+    private int lifeCounter;
+
+    private Text lifeText;
+
+    public GameObject GameOver;
+
+    public float WaitGameOver;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        lifeText = GetComponent<Text>();
+        lifeCounter = startingLives;
+
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(lifeCounter < 1)
+        {
+            GameOver.SetActive(true);
+            Time.timeScale = 0.1f;
+        }
+        
+        lifeText.text = "x " + lifeCounter;
+
+        if(GameOver.activeSelf)
+        {
+            WaitGameOver -= Time.deltaTime;
+        }
+
+        if(WaitGameOver < 0)
+        {
+            Time.timeScale = 1f;
+            SceneManager.LoadScene("MainMenu");
+        }
+    }
+
+    public void LifeUP()
+    {
+        lifeCounter++;
+    }
+
+    public void LifeDOWN()
+    {
+        lifeCounter--;
+    }
+}
