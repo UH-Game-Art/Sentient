9f9df7497d410cbead36e978bddb82a2df9669bc
KiraKamii <70298396+KiraKamii@users.noreply.github.com>
2020-11-15 15:49:18 -0600

    Main Menu updated
    Added volume slider and options menu need to finalize with art assets by Daniel A.

---
+++ Assets/Scripts/Menus/MainMenu.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Audio;
+public class MainMenu : MonoBehaviour
+{
+    public AudioMixer audioMixer;
+   public void PlayGame()
+   {
+       Debug.Log("Play Game");
+       PlayMenuSound();
+       SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+   }
+
+   public void QuitGame()
+   {
+       Debug.Log("Quit Game");
+       PlayMenuSound();
+       Application.Quit();
+   }
+
+   public void SetVolume(float volume)
+   {
+       audioMixer.SetFloat("Volume", volume);
+   }
+
+   public void PlayMenuSound()
+   {
+       AudioManager.instance.PlaySound(AudioManager.instance.menu_clicking, 0.8f);
+   }
+}
