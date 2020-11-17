98263b1d1d5f19a6f48fd09c3bad14d02a29d2bb
Hin Vuong <54330598+HinV@users.noreply.github.com>
2020-11-16 23:42:14 -0600

    Update

--- Assets/Scripts/Background/FrameSwitch.cs
+++ Assets/Scripts/Background/FrameSwitch.cs
@@ -6,18 +6,20 @@ public class FrameSwitch : MonoBehaviour
 {
     public GameObject frame1;
     public GameObject frame2;
-        
+            
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (frame1.active == true)
         {
             frame1.SetActive(false);
             frame2.SetActive(true);
+            
         }
         else if (frame1.active == false)
         {
             frame1.SetActive(true);
             frame2.SetActive(false);
+
         }
     }
 
---
+++ Assets/Scripts/portal/TeleportationScript.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeleportationScript : MonoBehaviour
+{
+	public GameObject Portal;
+	public GameObject Player;
+
+
+	// Use this for initialization
+	void Start()
+	{
+
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+
+	}
+
+	public void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.gameObject.tag == "Player")
+		{
+			StartCoroutine(Teleport());
+
+		}
+	}
+
+
+	IEnumerator Teleport()
+	{
+		yield return new WaitForSeconds(0.5f);
+		Player.transform.position = new Vector2(Portal.transform.position.x, Portal.transform.position.y);
+		yield return new WaitForSeconds(1f);
+		Destroy(Portal);
+	}
+}
+
