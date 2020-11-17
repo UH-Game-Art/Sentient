8a1c63d55b91c962261b182d4b8a479ff097acf6
jesussilverioe <jesussilverioe@hotmail.com>
2020-11-16 21:59:23 -0600

    Directional Aiming

--- Assets/Scripts/Character/Weapon.cs
+++ Assets/Scripts/Character/Weapon.cs
@@ -1,27 +1,52 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class Weapon : MonoBehaviour
 {
-
-    public Transform firePoint;
+    private Transform aimTransform;
+    public Transform firePoint; //point of fire from gun
+    public int damage = 10; //damage enemy takes 
     public GameObject bulletPrefab;
-    
+    public float fireRate = 0.5f;
+    private float nextFire = 0.0f;
+
+
+
+    private void Awake() {
+        aimTransform = transform.Find("FirePoint");
+    }
+
+
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetButtonDown("Fire1"))
+
+        if (Input.GetKey(KeyCode.Mouse0) && Time.time > nextFire)
         {
-            AudioManager.instance.PlaySound(AudioManager.instance.player_bullet, 0.5f);
-            Shoot();
+            nextFire = Time.time + fireRate;
+            Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
         }
+        HandleAiming();
+
     }
 
-    
-    void Shoot ()
+    private void HandleAiming()
     {
-        Instantiate(bulletPrefab,firePoint.position, firePoint.rotation);
+        //Get the Screen positions of the object
+        Vector2 positionOnScreen = Camera.main.WorldToViewportPoint(transform.position);
+
+        //Get the Screen position of the mouse
+        Vector2 mouseOnScreen = (Vector2)Camera.main.ScreenToViewportPoint(Input.mousePosition);
+
+        //Get the angle between the points
+        float angle = AngleBetweenTwoPoints(positionOnScreen, mouseOnScreen) - 180f;
+
+        //Ta Daaa
+        aimTransform.rotation = Quaternion.Euler(new Vector3(0f, 0f, angle));
+    }
+
+    float AngleBetweenTwoPoints(Vector3 a, Vector3 b)
+    {
+        return Mathf.Atan2(a.y - b.y, a.x - b.x) * Mathf.Rad2Deg;
     }
 }
