e33df6ee748ed170a43469a5732a90e07bd242bf
Aakartion <aakartionsimkhada@gmail.com>
2020-11-22 17:33:16 -0600

    script created

---
+++ Assets/Scripts/Inventory/Item.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item
+{
+    public enum ItemType
+    {
+        Gun,
+        HealthPotion,
+        Coin,
+        Medkit,
+    }
+
+    public ItemType itemType;
+    public int amount;
+}
+
