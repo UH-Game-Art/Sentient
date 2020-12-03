a57ea4bcd223f61089c7443be9a0f441b72ec190
KiraKamii <70298396+KiraKamii@users.noreply.github.com>
2020-12-02 18:37:34 -0600

    Ending Done
    Adding fading in and out text to end the story and roll into credits

---
+++ Assets/Scripts/Menus/CreditWait.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreditWait : MonoBehaviour
+{
+    
+    public GameObject Credits;
+    public float WaitTillCredits;
+    void Start()
+    {
+        StartCoroutine(WaitForCredits());
+    }
+
+    
+    IEnumerator WaitForCredits()
+    {
+        yield return new WaitForSeconds(WaitTillCredits);
+        Credits.SetActive(true);
+    }
+}
---
+++ Assets/Scripts/Menus/Fader.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Fader : MonoBehaviour
+{
+   public CanvasGroup uiElement;
+   public float TimeToFadeIn;
+   public float TimeToFadeOut;
+   
+   void Start()
+   {
+       StartCoroutine(WaitToFadeIn());
+       StartCoroutine(WaitToFadeOut());
+   }
+   public void FadeIn()
+   {
+       StartCoroutine(FadeCanvasGroup(uiElement,uiElement.alpha,1));
+   }
+   public void FadeOut()
+   {
+       StartCoroutine(FadeCanvasGroup(uiElement,uiElement.alpha,0));
+   }
+   public IEnumerator FadeCanvasGroup(CanvasGroup canvasGroup,float start, float end, float lerpTime = 1.0f)
+   {
+       
+       float timeStartedLerping = Time.time;
+       float timeSinceStarted = Time.time - timeStartedLerping;
+       float percentComplete = timeSinceStarted / lerpTime;
+       while(true)
+       {
+           timeSinceStarted = Time.time - timeStartedLerping;
+           percentComplete = timeSinceStarted / lerpTime;
+           
+           float currentValue = Mathf.Lerp(start,end,percentComplete);
+           
+           canvasGroup.alpha = currentValue;
+           
+           if(percentComplete >=1) break;
+           yield return new WaitForEndOfFrame();
+       }
+   }
+    IEnumerator WaitToFadeIn()
+    {
+       yield return new WaitForSeconds(TimeToFadeIn);
+       Debug.Log("FadeIn");
+       FadeIn();
+    }
+     IEnumerator WaitToFadeOut()
+    {
+       Debug.Log("FadeOut");
+       yield return new WaitForSeconds(TimeToFadeOut);
+       FadeOut();
+    }
+}
