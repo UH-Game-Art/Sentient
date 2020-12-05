414aea1bb60cbcb8b95e6d3a34194d0b83b51250
Edwin Campuzano <campuzano.edwin1995@gmail.com>
2020-12-05 16:23:12 -0600

    Player jumps only twice

--- Assets/Scripts/Character/PlayerMovement2.cs
+++ Assets/Scripts/Character/PlayerMovement2.cs
@@ -9,37 +9,39 @@ public class PlayerMovement2 : MonoBehaviour
 
     public Animator animator;
     public Rigidbody2D r2;
+    public Transform feet;
+    [SerializeField] private LayerMask whatIsGround;
 
     float horizontalMove = 0f;
     public float runSpeed = 40f;
-
+    private int extraJumps = 1;
+    private int jumps = 1;
     bool jump = false;
     bool isGrounded = false;
     bool crouch = false;
+
     // Update is called once per frame
 
 
 
     void Start()
     {
-
-        r2 = gameObject.GetComponent<Rigidbody2D>();
+        jumps = extraJumps;
+        feet = this.transform.Find("right_foot");
+        r2 = this.GetComponentInParent<Rigidbody2D>();
     }
     void Update()
     {
-        
-     
+        isGrounded = Physics2D.OverlapCircle(feet.position, 0.5f, whatIsGround);
         horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
         animator.SetFloat("Speed", Mathf.Abs(horizontalMove));
-
-        if (Input.GetButtonDown("Jump"))
+        if (Input.GetButtonDown("Jump") && jumps > 0)
         {
-
             AudioManager.instance.PlaySound(AudioManager.instance.jump, 1);
             jump = true;
             animator.SetTrigger("takeOff");
             animator.SetBool("IsJumping", true);
-
+            jumps--;
         }
 
         if (Input.GetButtonDown("Crouch"))
@@ -49,6 +51,11 @@ public class PlayerMovement2 : MonoBehaviour
         else if (Input.GetButtonUp("Crouch"))
         {
             crouch = false;
+        }
+
+        if (isGrounded)
+        {
+            jumps = extraJumps;
         }
     }
 
@@ -61,12 +68,10 @@ public class PlayerMovement2 : MonoBehaviour
     {
         controller.Move(horizontalMove * Time.fixedDeltaTime, crouch, jump);
         jump = false;
-
     }
 
     public void Knockback(float Knockpow, Vector2 Knockdir)
     {
-
         r2.velocity = new Vector2(0, 0);
         r2.AddForce(new Vector2(Knockdir.x * Knockpow, Knockdir.y * Knockpow));
     }
