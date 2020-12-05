0eb12ecb7b86ec714c6d24600200b69d9a17d780
Edwin Campuzano <campuzano.edwin1995@gmail.com>
2020-12-05 03:24:20 -0600

    Death animation for Mecha

--- Assets/Scripts/Enemy/Turret.cs
+++ Assets/Scripts/Enemy/Turret.cs
@@ -16,6 +16,7 @@ public class Turret : MonoBehaviour
     private Transform player;
     private bool playerWithinRange = false;
     private bool playerFound = false;
+    private bool isDead = false;
 
     // Start is called before the first frame update
     void Start()
@@ -30,7 +31,7 @@ public class Turret : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (playerFound && withinRange())
+        if (playerFound && withinRange() && !isDead)
         {
             HandleAiming();
             if (!inCoolDown)
