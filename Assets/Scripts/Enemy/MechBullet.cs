3c788a28a40aece79b08ab623a596b3adf7a9897
Edwin Campuzano <campuzano.edwin1995@gmail.com>
2020-12-05 13:21:45 -0600

    Updated Bullet prefab damage player

--- Assets/Scripts/Enemy/MechBullet.cs
+++ Assets/Scripts/Enemy/MechBullet.cs
@@ -4,13 +4,14 @@ using UnityEngine;
 
 public class MechBullet : MonoBehaviour
 {
-    
+
     public float speed;
     public int damage;
     private Rigidbody2D rb;
     public PlayerMovement2 player;
     public float Bullet_Time = 0.5f;
     private bool playerFound = false;
+    public PlayerHealth pb;
 
     // Start is called before the first frame update
     void Start()
@@ -18,6 +19,7 @@ public class MechBullet : MonoBehaviour
         rb = GetComponent<Rigidbody2D>();
         rb.velocity = transform.right * speed;
         Destroy(gameObject, Bullet_Time);
+        pb = FindObjectOfType<PlayerHealth>();
         if (player = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerMovement2>())
         {
             playerFound = true;
@@ -28,12 +30,12 @@ public class MechBullet : MonoBehaviour
     {
         if (collision.CompareTag("Player"))
         {
-            collision.SendMessageUpwards("damage", damage);
             if (playerFound)
             {
                 player.Knockback(100f, player.transform.position);
+                pb.damage(damage);
             }
-            Destroy(gameObject, 0.1f); // destroy bullet if hit mob 
+            Destroy(gameObject, 0.01f); // destroy bullet if hit mob 
         }
     }
 }
