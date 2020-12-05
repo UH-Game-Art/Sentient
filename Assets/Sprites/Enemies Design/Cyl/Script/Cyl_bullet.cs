d9da5aec7922e6a543e221a7d77851e0d419d6a4
vuquockhanh124 <54862143+vuquockhanh124@users.noreply.github.com>
2020-12-05 11:21:13 -0600

    update cyl prefab

--- Assets/Sprites/Enemies Design/Cyl/Script/Cyl_bullet.cs
+++ Assets/Sprites/Enemies Design/Cyl/Script/Cyl_bullet.cs
@@ -8,7 +8,7 @@ public class Cyl_bullet : MonoBehaviour
     public float speed=2;
     public int damage=2;
     public float bullet_duration = 0.2f;
-    //public Rigidbody2D rb;
+    public Rigidbody2D rb;
 
     private Transform player;
     private Vector2 target;
@@ -20,15 +20,15 @@ public class Cyl_bullet : MonoBehaviour
     {
         player = GameObject.FindGameObjectWithTag("Player").transform;
         target = new Vector2(player.position.x, player.position.y);
-
+        rb.velocity = transform.right * speed;
         Destroy(gameObject, bullet_duration);
-        //   rb.velocity = transform.right * speed;
+        
     }
 
 
     void Update()
     {
-        transform.position = Vector2.MoveTowards(transform.position, target, speed * Time.deltaTime);
+        transform.position = Vector2.MoveTowards(transform.position, player.transform.position, speed * Time.deltaTime);
       
 
     }
