d00dce83b2b1547743b1025bfc0ed8645dee4d4c
yoretessma <51445179+yoretessma@users.noreply.github.com>
2020-12-05 14:42:17 -0600

    Add files via upload

---
+++ Assets/Scripts/Character/WeaponSwap.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponSwap : MonoBehaviour
+{
+    public GameObject pistol;
+    public GameObject gun;
+    public int Weapon = 0;
+
+    void Update()
+    {
+        SwapWeapons();
+       
+    }
+
+    void SwapWeapons()
+    {
+        if (Input.GetButtonDown("Swap"))
+        {
+            if (Weapon == 0)
+            {
+                pistol.gameObject.SetActive(false);
+                gun.gameObject.SetActive(true);
+                Weapon = 1;
+            }
+            else
+            {
+                gun.gameObject.SetActive(false);
+                pistol.gameObject.SetActive(true);
+                Weapon = 0;
+            }
+        }
+    }
+}
