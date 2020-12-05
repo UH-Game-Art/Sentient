58de8720079af9da91fe1fe01df270f7693844b1
yoretessma <yoretessma@gmail.com>
2020-12-05 10:38:44 -0600

    update

--- Assets/Scripts/Character/Bullet.cs
+++ Assets/Scripts/Character/Bullet.cs
@@ -8,10 +8,12 @@ public class Bullet : MonoBehaviour
     public int damage;
     public Rigidbody2D rb;
     public float Bullet_Time = 0.5f;
+    public boss_health bh;
     // Start is called before the first frame update
     void Start()
     {
         rb.velocity = transform.right * speed;
+        bh = FindObjectOfType<boss_health>();
         Destroy(gameObject, Bullet_Time);
     }
 
@@ -22,6 +24,11 @@ public class Bullet : MonoBehaviour
         {
             collision.SendMessageUpwards("Damage", damage);
             Destroy(gameObject,0.1f); // destroy bullet if hit mob 
+        }
+        if (collision.gameObject.tag == "Boss")
+        {
+            bh.BossDamage(5);
+            Destroy(gameObject, 0.1f); // destroy bullet if hit mob 
         }
 
 
