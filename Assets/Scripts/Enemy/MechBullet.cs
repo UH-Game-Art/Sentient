cac7f9c25b08f8fe0a9d7732a17c60d2c05dec01
Edwin Campuzano <campuzano.edwin1995@gmail.com>
2020-12-05 00:19:48 -0600

    New Bullet Script for mecha

--- Assets/Scripts/Enemy/MechBullet.cs
+++ Assets/Scripts/Enemy/MechBullet.cs
@@ -2,12 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Bullet : MonoBehaviour
+public class MechBullet : MonoBehaviour
 {
     public float speed;
     public int damage;
     public Rigidbody2D rb;
     public float Bullet_Time = 0.5f;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -17,16 +18,10 @@ public class Bullet : MonoBehaviour
 
     void OnTriggerEnter2D(Collider2D collision)
     {
-
         if (collision.CompareTag("Enemy"))
         {
             collision.SendMessageUpwards("Damage", damage);
-            Destroy(gameObject,0.1f); // destroy bullet if hit mob 
+            Destroy(gameObject, 0.1f); // destroy bullet if hit mob 
         }
-
-
     }
-
-
-   
 }
