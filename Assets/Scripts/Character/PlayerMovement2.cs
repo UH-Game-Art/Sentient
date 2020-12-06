ec3e937393d4f7b9eb71b81a0f905d69a1f64553
Hin Vuong <54330598+HinV@users.noreply.github.com>
2020-12-05 18:27:01 -0600

    Revert "Player jumps only twice"
    This reverts commit 414aea1bb60cbcb8b95e6d3a34194d0b83b51250.

--- Assets/Scripts/Character/PlayerMovement2.cs
+++ Assets/Scripts/Character/PlayerMovement2.cs
@@ -9,39 +9,37 @@ public class PlayerMovement2 : MonoBehaviour
 
     public Animator animator;
     public Rigidbody2D r2;
-    public Transform feet;
-    [SerializeField] private LayerMask whatIsGround;
 
     float horizontalMove = 0f;
     public float runSpeed = 40f;
-    private int extraJumps = 1;
-    private int jumps = 1;
+
     bool jump = false;
     bool isGrounded = false;
     bool crouch = false;
-
     // Update is called once per frame
 
 
 
     void Start()
     {
-        jumps = extraJumps;
-        feet = this.transform.Find("right_foot");
-        r2 = this.GetComponentInParent<Rigidbody2D>();
+
+        r2 = gameObject.GetComponent<Rigidbody2D>();
     }
     void Update()
     {
-        isGrounded = Physics2D.OverlapCircle(feet.position, 0.5f, whatIsGround);
+        
+     
         horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
         animator.SetFloat("Speed", Mathf.Abs(horizontalMove));
-        if (Input.GetButtonDown("Jump") && jumps > 0)
+
+        if (Input.GetButtonDown("Jump"))
         {
+
             AudioManager.instance.PlaySound(AudioManager.instance.jump, 1);
             jump = true;
             animator.SetTrigger("takeOff");
             animator.SetBool("IsJumping", true);
-            jumps--;
+
         }
 
         if (Input.GetButtonDown("Crouch"))
@@ -51,11 +49,6 @@ public class PlayerMovement2 : MonoBehaviour
         else if (Input.GetButtonUp("Crouch"))
         {
             crouch = false;
-        }
-
-        if (isGrounded)
-        {
-            jumps = extraJumps;
         }
     }
 
@@ -68,10 +61,12 @@ public class PlayerMovement2 : MonoBehaviour
     {
         controller.Move(horizontalMove * Time.fixedDeltaTime, crouch, jump);
         jump = false;
+
     }
 
     public void Knockback(float Knockpow, Vector2 Knockdir)
     {
+
         r2.velocity = new Vector2(0, 0);
         r2.AddForce(new Vector2(Knockdir.x * Knockpow, Knockdir.y * Knockpow));
     }
